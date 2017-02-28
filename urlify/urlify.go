90af14a4fda5201681d09338147a51ebfe7d5d15
sangeet <sangeet.sdahal@gmail.com>
2017-02-28 14:51:02 -0600

    problem/urlify === completed tests for urlifying string

---
+++ urlify/urlify.go
@@ -0,0 +1,29 @@
+package url
+
+const zero = '0'
+const two = '2'
+const percentage = '%'
+
+// Urlify replaces all the ocurrances of space in between
+// characters with "%20". The url should have space for the
+// "%20" at it back.
+func Urlify(url []uint8, length int){
+	space := uint8(' ')
+	i := length - 1
+	for i >=0 && url[i] == space{
+		i--
+	}
+	j := length	- 1
+	for i = i ; i >= 0 ; i--{
+		if url[i] == space{
+			url[ j ] = zero
+			url[ j - 1 ] = two
+			url[ j - 2 ] = percentage
+			j = j - 3
+		}else{
+			url[j] = url[i]
+			j--
+		}
+	}
+
+}
---
+++ urlify/urlify_test.go
@@ -0,0 +1,46 @@
+package url
+
+import "testing"
+
+// Note we have enough space on an array to replace space by %20
+// We do not do any validation here
+// Validation shoudl be simple enough though
+// if there are five spaces, there should be additional 10 extra space at back
+
+func convertToString(url []uint8)string{
+	received := ""
+	for _, v := range url{
+		received = received + string(v)
+	}
+	return received
+}
+
+func TestCharArrayWithSpace(t *testing.T){
+	url := []uint8{'a',' ','b',' ','c','e','f','g',' ',' ',' ',' '}
+	expected := "a%20b%20cefg"
+	Urlify(url, len(url))
+	received := convertToString(url)
+	if expected != received{
+		t.Error("Expected ",expected," but got", received)
+	}
+}
+
+func TestCharArrayWithoutSpace(t *testing.T){
+	url := []uint8{'a','b','c','e','f','g'}
+	expected := "abcefg"
+	Urlify(url, len(url))
+	received := convertToString(url)
+	if expected != received{
+		t.Error("Expected ",expected," but got", received)
+	}
+}
+
+func TestEmptyCharArray(t *testing.T){
+	url := []uint8{}
+	expected := ""
+	Urlify(url, len(url))
+	received := convertToString(url)
+	if expected != received{
+		t.Error("Expected ",expected," but got", received)
+	}
+}
