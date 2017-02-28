0fb8754a6bcba5fcde0d687cb35e54f1ef5c1e3c
sangeet <sangeet.sdahal@gmail.com>
2017-02-28 10:42:26 -0600

    hamming_distance===refactored

--- hamming_distance/hamming_distance.go
+++ hamming_distance/hamming_distance.go
@@ -1,5 +1,6 @@
 package hammingdistance
 
+// returns how many bits are set on a number
 func GetNoOfOnes(no int) int {
 		if no == 0 {
 			return 0
@@ -8,6 +9,8 @@ func GetNoOfOnes(no int) int {
 		return 1 + GetNoOfOnes(no & (no - 1))	
 }
 
+// return hamming distance
 func GetHammingDistance(a int, b int) int {
+	// xor operation between sets those bit which are different
 	return GetNoOfOnes(a^b)
 }
