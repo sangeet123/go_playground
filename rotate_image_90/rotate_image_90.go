be150275860b14e2010a1a2e2b1ff5a19c6aceda
sangeet <sangeet.sdahal@gmail.com>
2017-03-11 14:17:33 -0600

    rotate-image-90===added tests and three ways of performing rotation

---
+++ rotate_image_90/rotate_image_90.go
@@ -0,0 +1,89 @@
+package rotateimage
+
+
+// This function works for rectangular image where
+// height and width are not of equal dimension
+func rotateimageby90(img [][]int)[][]int{
+	if len(img) == 0 {
+		return img
+	}
+
+	rotatedimg :=  make([][]int, len(img[0]))
+	for i :=0; i<len(img) ; i++{
+		for j := 0; j<len(img[i]); j++{
+			rotatedimg[j] = append([]int{img[i][j]}, rotatedimg[j]...)
+		} 
+	}
+	return rotatedimg
+}
+
+
+func assert_square_matrix(arr[][]int){
+	is_square := len(arr) == 0 || len(arr) == len(arr[0]);
+
+	if !is_square{
+		panic("not a square matrix")
+	}
+}
+
+
+// This function is inplace for square image
+// There no return type since original image will be
+// modified
+func rotatedimgby90inplace(img [][]int){
+	assert_square_matrix(img)
+	start := 0
+	end := len(img) - 1
+	for start<end{
+		for i := 0; i < end - start; i++{
+			// save the initial element
+			start_element := img[start][start + i]
+			img[start][start + i] = img[end - i ][start]
+			img[end - i][start] = img[end][end - i]
+			img[end][end - i] = img[start+i][end]
+			img[start+i][end] = start_element
+		}
+		start++
+		end--
+	}
+
+}
+
+// This function is inplace for square image
+// There no return type since original image will be
+// modified
+func rotatedimgby90inplaceUsingTranspose(img [][]int){
+	assert_square_matrix(img)
+	perform_inplace_matrix_transpose(img)
+	swap_matrix_columns(img)
+}
+
+func swap_matrix_columns(img[][]int){
+	for i:=0;i<len(img);i++{
+		reverse_array(img[i])
+	}
+}
+
+func reverse_array(arr []int){
+	i := 0
+	j := len(arr) - 1
+	for i<j{
+		temp := arr[i]
+		arr[i] = arr[j]
+		arr[j] = temp
+		i++
+		j--
+	}
+}
+
+//Only for square matrix
+func perform_inplace_matrix_transpose(img[][]int){
+	assert_square_matrix(img)
+	for i:=0;i<len(img);i++{
+		for j:=i;j<len(img);j++{
+			temp := img[i][j]
+			img[i][j] = img[j][i]
+			img[j][i] = temp
+		}
+	}
+}
---
+++ rotate_image_90/rotate_image_90_test.go
@@ -0,0 +1,179 @@
+package rotateimage
+
+import "testing"
+import "reflect"
+
+func TestRotateImageEmptyArray(t *testing.T){
+	input := [][]int{}
+	output := [][]int{}
+	received := rotateimageby90(input)
+
+	if len(received) != 0 {
+		t.Error("Expected ", output, " but got ", received)
+	}
+}
+
+func TestRotateImageSingleElementArray(t *testing.T){
+	input := [][]int{{1}}
+	output := [][]int{{1}}
+	received := rotateimageby90(input)
+
+	if !reflect.DeepEqual(output, received){
+		t.Error("Expected ", output, " but got ", received)
+	}
+}
+
+func TestRotateImageSingleRowArray(t *testing.T){
+	input := [][]int{{1,2,3,4}}
+	output := [][]int{{1},{2},{3},{4}}
+	received := rotateimageby90(input)
+
+	if !reflect.DeepEqual(output, received){
+		t.Error("Expected ", output, " but got ", received)
+	}
+}
+
+func TestRotateImageSingleColumnArray(t *testing.T){
+	input := [][]int{{1},{2},{3},{4}}
+	output := [][]int{{4,3,2,1}}
+	received := rotateimageby90(input)
+
+	if !reflect.DeepEqual(output, received){
+		t.Error("Expected ", output, " but got ", received)
+	}
+}
+
+func TestRotateImageMultiDimensionalArray(t *testing.T){
+	input := [][]int{{1,2,3},{4,5,6}}
+	output := [][]int{{4, 1},{5,2},{6, 3}}
+	received := rotateimageby90(input)
+
+	if !reflect.DeepEqual(output, received){
+		t.Error("Expected ", output, " but got ", received)
+	}
+}
+
+func TestRotateImageMultiDimensionalSquareArrayEvenDimension(t *testing.T){
+	input := [][]int{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}}
+	output := [][]int{{13,9,5,1},{14,10,6,2},{15,11,7,3},{16,12,8,4}}
+	received := rotateimageby90(input)
+
+	if !reflect.DeepEqual(output, received){
+		t.Error("Expected ", output, " but got ", received)
+	}
+}
+
+func TestRotateImageInplaceMultiOneElementSquareArray(t *testing.T){
+	input := [][]int{{1}}
+	output := [][]int{{1}}
+	rotatedimgby90inplace(input)
+	if !reflect.DeepEqual(output, input){
+		t.Error("Expected ", output, " but got ", input)
+	}
+}
+
+func TestRotateImageInplaceMultiDimensionalSquareArrayOddDimension(t *testing.T){
+	//input
+	//1,2,3                7,4,1
+	//4,5,6    =========>  8,5,2
+	//7,8,9				   9,6,3
+	input := [][]int{{1,2,3},{4,5,6},{7,8,9}}
+	output := [][]int{{7,4,1},{8,5,2},{9,6,3}}
+	rotatedimgby90inplace(input)
+
+	if !reflect.DeepEqual(output, input){
+		t.Error("Expected ", output, " but got ", input)
+	}
+}
+
+func TestRotateImageInplaceMultiDimensionalSquareArrayEvenDimension(t *testing.T){
+	input := [][]int{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}}
+	output := [][]int{{13,9,5,1},{14,10,6,2},{15,11,7,3},{16,12,8,4}}
+	rotatedimgby90inplace(input)
+
+	if !reflect.DeepEqual(output, input){
+		t.Error("Expected ", output, " but got ", input)
+	}
+}
+
+
+func TestRotateImageInplaceMultiDimensionalSquareArrayOddSevenDimension(t *testing.T){
+	// input
+	// 1,  2,   3,  4,  5,  6,  7       			 15, 8,  1, 22,  15,  8,  1
+	// 8,  9,   10, 11, 12, 13, 14    	=========>   16, 9,  2, 23,  16,  9,  2
+	// 15, 16,  17, 18, 19, 20, 21				     17, 10, 3, 24,  17,  10, 3
+	// 22, 23,  24, 25, 26, 27, 28					 18, 11, 4, 25,  18,  11  4
+	// 1,  2,   3,  4,  5,  6,  7       			 19, 12, 5, 26,  19,  12, 5
+	// 8,  9,   10, 11, 12, 13, 14    	=========>   20, 13, 6, 27,  20,  13, 6
+	// 15, 16,  17, 18, 19, 20, 21				     21, 14, 7, 28,  21,  14, 7
+
+	input:=[][]int{{1,2,3,4,5,6,7},{8,9,10,11,12,13,14},{15,16,17,18,19,20,21},{22,23,24,25,26,27,28},{1,2,3,4,5,6,7},{8,9,10,11,12,13,14},{15,16,17,18,19,20,21}}
+	output:=[][]int{{15,8,1,22,15,8,1},{16,9,2,23,16,9,2},{17,10,3,24,17,10,3},{18,11,4,25,18,11,4},{19,12,5,26,19,12,5},{20,13,6,27,20,13,6},{21,14,7,28,21,14,7}}
+	rotatedimgby90inplace(input)
+
+	if !reflect.DeepEqual(output, input){
+		t.Error("Expected ", output, " but got ", input)
+	}
+}
+
+
+//============================================O(2n) less efficient ===============================================================
+
+func TestRotateImageUsingTransposeMultiOneElementSquareArray(t *testing.T){
+	input := [][]int{{1}}
+	output := [][]int{{1}}
+	rotatedimgby90inplaceUsingTranspose(input)
+	if !reflect.DeepEqual(output, input){
+		t.Error("Expected ", output, " but got ", input)
+	}
+}
+
+func TestRotateImageUsingTransposeMultiDimensionalSquareArrayOddDimension(t *testing.T){
+	//input
+	//1,2,3                7,4,1
+	//4,5,6    =========>  8,5,2
+	//7,8,9				   9,6,3
+	input := [][]int{{1,2,3},{4,5,6},{7,8,9}}
+	output := [][]int{{7,4,1},{8,5,2},{9,6,3}}
+	rotatedimgby90inplaceUsingTranspose(input)
+
+	if !reflect.DeepEqual(output, input){
+		t.Error("Expected ", output, " but got ", input)
+	}
+}
+
+func TestRotateImageUsingTransposeMultiDimensionalSquareArrayEvenDimension(t *testing.T){
+	input := [][]int{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}}
+	output := [][]int{{13,9,5,1},{14,10,6,2},{15,11,7,3},{16,12,8,4}}
+	rotatedimgby90inplaceUsingTranspose(input)
+
+	if !reflect.DeepEqual(output, input){
+		t.Error("Expected ", output, " but got ", input)
+	}
+}
+
+
+func TestRotateImageUsingTransposeMultiDimensionalSquareArrayOddSevenDimension(t *testing.T){
+	// input
+	// 1,  2,   3,  4,  5,  6,  7       			 15, 8,  1, 22,  15,  8,  1
+	// 8,  9,   10, 11, 12, 13, 14    	=========>   16, 9,  2, 23,  16,  9,  2
+	// 15, 16,  17, 18, 19, 20, 21				     17, 10, 3, 24,  17,  10, 3
+	// 22, 23,  24, 25, 26, 27, 28					 18, 11, 4, 25,  18,  11  4
+	// 1,  2,   3,  4,  5,  6,  7       			 19, 12, 5, 26,  19,  12, 5
+	// 8,  9,   10, 11, 12, 13, 14    	=========>   20, 13, 6, 27,  20,  13, 6
+	// 15, 16,  17, 18, 19, 20, 21				     21, 14, 7, 28,  21,  14, 7
+
+	input:=[][]int{{1,2,3,4,5,6,7},{8,9,10,11,12,13,14},{15,16,17,18,19,20,21},{22,23,24,25,26,27,28},{1,2,3,4,5,6,7},{8,9,10,11,12,13,14},{15,16,17,18,19,20,21}}
+	output:=[][]int{{15,8,1,22,15,8,1},{16,9,2,23,16,9,2},{17,10,3,24,17,10,3},{18,11,4,25,18,11,4},{19,12,5,26,19,12,5},{20,13,6,27,20,13,6},{21,14,7,28,21,14,7}}
+	rotatedimgby90inplaceUsingTranspose(input)
+
+	if !reflect.DeepEqual(output, input){
+		t.Error("Expected ", output, " but got ", input)
+	}
+}
+
+
+
+
+
+
