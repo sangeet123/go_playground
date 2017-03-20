3ecb2929340a25ac3225068b244d82dcdca46ba3
sangeet <sangeet.sdahal@gmail.com>
2017-03-20 12:07:29 -0500

    teemo_attack===Added code for teemo_attack

---
+++ teemo_attack/teemo_attack.go
@@ -0,0 +1,24 @@
+package teemo
+
+func findPoisonedDuration(timeSeries []int, duration int) int {
+    
+    poisoned_for := 0
+    affect_till := 0
+    
+    for i := 0 ; i < len(timeSeries) ; i++{
+        next_affect := timeSeries[i] + duration
+        
+        if next_affect <= affect_till{
+            continue
+        }
+        
+        if timeSeries[i] >= affect_till{
+            poisoned_for += duration    
+        }else{
+            poisoned_for += next_affect - affect_till
+        }
+        affect_till = next_affect
+    }
+    
+    return poisoned_for
+}
