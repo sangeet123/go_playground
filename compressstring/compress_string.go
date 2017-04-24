c59e83c672909e04be8e4d701cf534ad94363661
sangeet <sangeet.sdahal@gmail.com>
2017-04-24 12:59:59 -0500

    refactored===Added utf8 implementation of compress string

--- compressstring/compress_string.go
+++ compressstring/compress_string.go
@@ -1,8 +1,13 @@
 package compressstring
 
-import "strconv"
+import (
+	"bytes"
+	"fmt"
+	"unicode/utf8"
+)
 
 // This is O(2n) solution
+// This works for utf8 encoding scheme
 func Compress(word string) string {
 	char_map := map[rune]int{}
 
@@ -10,16 +15,18 @@ func Compress(word string) string {
 		char_map[val]++
 	}
 
-	compressed := ""
+	var compressed bytes.Buffer
 	for _, val := range word {
-		if _, ok := char_map[val]; ok {
-			compressed = compressed + string(val) + strconv.Itoa(char_map[val])
+		if count, ok := char_map[val]; ok {
+			compressed.WriteString(string(val))
+			fmt.Fprintf(&compressed, "%d", count)
 		}
 		delete(char_map, val)
 	}
 
-	if len(compressed) < len(word) {
-		return compressed
+	compressedString := compressed.String()
+	if len(compressedString) < len(word) {
+		return compressedString
 	}
 
 	return word
@@ -32,20 +39,25 @@ func CompressEfficient(word string) string {
 	}
 
 	count := 1
-	compressed := ""
-	for i := 1; i < len(word) && len(compressed) < len(word); i++ {
-		if word[i] != word[i-1] {
-			compressed = string(word[i-1]) + strconv.Itoa(count)
+	var compressed bytes.Buffer
+	prevChar, width := utf8.DecodeRuneInString(word[0:])
+	for _, val := range word[width:] {
+		if val != prevChar {
+			compressed.WriteString(string(prevChar))
+			fmt.Fprintf(&compressed, "%d", count)
+			prevChar = val
 			count = 1
 		} else {
 			count++
 		}
 	}
 
-	compressed = compressed + string(word[len(word)-1]) + strconv.Itoa(count)
+	compressed.WriteString(string(prevChar))
+	fmt.Fprintf(&compressed, "%d", count)
+	compressedString := compressed.String()
 
-	if len(compressed) < len(word) {
-		return compressed
+	if len(compressedString) < len(word) {
+		return compressedString
 	}
 
 	return word
