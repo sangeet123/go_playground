e57e2d65dd2ea17691238983407852a54194c942
sangeet <sangeet.sdahal@gmail.com>
2017-04-27 10:16:12 -0500

    stack==formatted

--- test/stack/stack_test.go
+++ test/stack/stack_test.go
@@ -5,19 +5,18 @@ import (
 	"testing"
 )
 
-
 func TestStackOperations(t *testing.T) {
 	s := stack.NewStack()
 
-	for i := 0 ; i < 10 ; i++{
+	for i := 0; i < 10; i++ {
 		s.Push(i)
 	}
 
-	for i := 9 ; i >= 0 ; i--{
+	for i := 9; i >= 0; i-- {
 		data := s.Pop()
 
-		if data != i{
-			t.Error("Expected ", i , " but got ", data)
+		if data != i {
+			t.Error("Expected ", i, " but got ", data)
 		}
 	}
 
@@ -25,7 +24,7 @@ func TestStackOperations(t *testing.T) {
 		t.Error("Expected size of 0 but got ", s.Size())
 	}
 
-	poping from empty stack should result in error
+	//poping from empty stack should result in error
 	defer func() {
 		if r := recover(); r == nil {
 			t.Error("no error ocurred")
