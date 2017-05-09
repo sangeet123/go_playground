cc2d8c4f4d8fd713b21be26c4eefbee80acecd8a
sangeet <sangeet.sdahal@gmail.com>
2017-05-09 10:11:48 -0500

    tree===added test cases for preorder and postorder

--- test/tree/bstInt_test.go
+++ test/tree/bstInt_test.go
@@ -17,15 +17,27 @@ func comparator(a, b int) int {
 
 func TestCharArrayWithSpace(t *testing.T) {
 	nos := []int{4, 5, 3, 2, 7, 8, 1, 2}
-	expected := []int{1, 2, 2, 3, 4, 5, 7, 8}
+	expectedInOrder := []int{1, 2, 2, 3, 4, 5, 7, 8}
+	expectedPreOrder := []int{4, 3, 2, 1, 2, 5, 7, 8}
+	expectedPostOrder := []int{2, 1, 2, 3, 8, 7, 5, 4}
 	tre := tree.GetIntTree(comparator)
 	for _, val := range nos {
 		tre.Insert(val)
 	}
 
 	inorder := tre.InOrder()
-	if !reflect.DeepEqual(expected, inorder) {
-		t.Error("expected ", expected, " but received ", inorder)
+	if !reflect.DeepEqual(expectedInOrder, inorder) {
+		t.Error("expected inOrder ", expectedInOrder, " but received ", inorder)
+	}
+
+	preOrder := tre.PreOrder()
+	if !reflect.DeepEqual(expectedPreOrder, preOrder) {
+		t.Error("expected preOrder ", expectedPreOrder, " but received ", preOrder)
+	}
+
+	postOrder := tre.PostOrder()
+	if !reflect.DeepEqual(expectedPostOrder, postOrder) {
+		t.Error("expected postOrder ", expectedPostOrder, " but received ", postOrder)
 	}
 
 }
